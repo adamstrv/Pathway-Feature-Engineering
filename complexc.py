646f67006e0699b630194a0bc49538ceead60096
KnightOfApollo <gevorkjanlewon@gmail.com>
2024-12-03 10:44:01 +0100

    Commit 1

--- complexc.py
+++ complexc.py
@@ -1,1 +1,26 @@
 #Complex C
+
+# Import super class
+from signaling_entities import SignalingEntities
+
+class ComplexC(SignalingEntities):
+    def __init__(self, name:str, input1:float, input2:float):
+        # Call base class to define name variable
+        super().__init__(name)
+        # Store input data in variables
+        self.inputK2 = input1
+        self.inputK3 = input2
+    
+    def activate(self, threshold: float = 0.15):
+        # Set conditions for activation
+        if self.inputK2 > threshold and self.inputK3 > threshold:
+            self.output = (self.inputK2,self.inputK3)
+            #testing check
+            print(self.output)
+        # Print error message only when unnecessary
+        elif not (self.inputK2 > threshold and self.inputK3 > threshold) and self.inputK2 > 0 and self.inputK3 > 0:
+            self.output = 0
+            print(f"Error: The values of K2 ({self.inputK2:.2f}) and K3 ({self.inputK3:.2f}) do not both exceed the threshold.")
+            print(f"Needs to be a minimal of {threshold:.2f}.")
+        return self.output
+    
--- kinase3.py
+++ kinase3.py
@@ -1,1 +1,21 @@
 # Kinase, for K3
+
+# Import super class
+from signaling_entities import SignalingEntities
+
+class Kinase3(SignalingEntities):
+    def __init__(self, name:str, input:float):
+        # Call base class to define name variable
+        super().__init__(name)
+        # Store input data in variables
+        self.input = input
+
+    def activate(self, threshold:float=0.01):
+        # Activate kinase if P1 signal breaches threshold
+        if self.input > threshold:
+            self.output = self.input - threshold
+        # Print error message only when unnecessary
+        elif not (self.input > threshold) and self.input > 0:
+            self.output = 0
+            print("Error: The signal from P1 is not big enough for the kinase (K3) to breach through to the next signal.")
+        return self.output
